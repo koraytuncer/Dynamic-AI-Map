dbd0c2b68e25c44095f7a703abb4dc8bd80e27ad
koraytuncer <ktuncerr@gmail.com>
2025-01-05 00:22:55 +0300

    Build için gerekli değişiklikler yapıldı

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,6 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  
-  return {
-    plugins: [vue()],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
-    },
-    define: {
-      'process.env.VITE_OPENAI_API_KEY': JSON.stringify(env.VITE_OPENAI_API_KEY)
-    }
-  }
+export default defineConfig({
+  plugins: [vue()],
 }) 
